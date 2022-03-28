8a2533ed67e8c43f78fd7170531ebdef278cf408
Sainz Markel <msainz@ikerlan.es>
2022-03-28 12:04:01 +0200

    Upload

---
+++ init.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+swat-s1 init.py
+
+Run this script just once to create and init the sqlite table.
+"""
+
+from minicps.states import SQLiteState
+from utils import PATH, SCHEMA, SCHEMA_INIT
+
+
+if __name__ == "__main__":
+
+    SQLiteState._create(PATH, SCHEMA)
+    SQLiteState._init(PATH, SCHEMA_INIT)
